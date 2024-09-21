a3dc9a55c5e8c5aa81a163be954d1e530f7ab954
phuclehehe <103525830+phuclehehe@users.noreply.github.com>
2024-09-21 16:59:20 +0700

    update template

--- DonationInUniversity/src/main/java/com/example/DonationInUniversity/Controller/HomeController.java
+++ DonationInUniversity/src/main/java/com/example/DonationInUniversity/Controller/HomeController.java
@@ -2,21 +2,16 @@ package com.example.DonationInUniversity.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-    @GetMapping("/test")
-    public String test() {
-        return "success"; 
-    }
+
     @GetMapping("/")
     public String homepage() {
-        return "index"; 
+        return "index";
     }
-    @GetMapping("/test1")
-    public String homepage1() {
-        return "test"; 
+    @GetMapping("/auth-login-basic")
+    public String login() {
+        return "auth-login-basic";
     }
 }
