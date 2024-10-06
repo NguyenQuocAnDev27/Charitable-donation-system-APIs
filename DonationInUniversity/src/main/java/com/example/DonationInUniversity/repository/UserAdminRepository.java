c5c4b8965f0b9b96ff3bd076a48439100d1c6bd6
phuclehehe <103525830+phuclehehe@users.noreply.github.com>
2024-10-06 18:35:05 +0700

    fix bug project admin

--- DonationInUniversity/src/main/java/com/example/DonationInUniversity/repository/UserAdminRepository.java
+++ DonationInUniversity/src/main/java/com/example/DonationInUniversity/repository/UserAdminRepository.java
@@ -1,10 +1,15 @@
 package com.example.DonationInUniversity.repository;
 
+import com.example.DonationInUniversity.model.Role;
 import com.example.DonationInUniversity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserAdminRepository extends JpaRepository<User,Integer> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
+
+    List<User> findByRole(Role role);
 }
