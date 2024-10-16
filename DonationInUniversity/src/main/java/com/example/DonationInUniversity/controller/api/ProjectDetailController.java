2ceda9e99850be9934a6e472ffe52f337507b713
Kien-Huy <113742104+Kien-Huy@users.noreply.github.com>
2024-10-16 14:28:30 +0700

    fix post when move input

--- DonationInUniversity/src/main/java/com/example/DonationInUniversity/controller/api/ProjectDetailController.java
+++ DonationInUniversity/src/main/java/com/example/DonationInUniversity/controller/api/ProjectDetailController.java
@@ -1,31 +1,31 @@
-package com.example.DonationInUniversity.controller.api;
-
-
-import com.example.DonationInUniversity.model.MyCustomResponse;
-import com.example.DonationInUniversity.model.ProjectDetail;
-import com.example.DonationInUniversity.service.api.ProjectDetailService;
-import com.example.DonationInUniversity.service.api.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/project_detail")
-public class ProjectDetailController {
-    private final ProjectDetailService projectDetailService;
-
-    @Autowired
-    public ProjectDetailController(ProjectDetailService projectDetailService) {
-        this.projectDetailService = projectDetailService;
-    }
-
-    @GetMapping("/show")
-    public MyCustomResponse<ProjectDetail> getProjectDetailById(@RequestParam Integer projectId) {
-        ProjectDetail projectDetail = projectDetailService.getProjectDetails(projectId);
-
-        return new MyCustomResponse<>(
-                200,
-                "Project details retrieved successfully",
-                projectDetail
-        );
-    }
-}
+//package com.example.DonationInUniversity.controller.api;
+//
+//
+//import com.example.DonationInUniversity.model.MyCustomResponse;
+//import com.example.DonationInUniversity.model.ProjectDetail;
+//import com.example.DonationInUniversity.service.api.ProjectDetailService;
+//import com.example.DonationInUniversity.service.api.ProjectService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/project_detail")
+//public class ProjectDetailController {
+//    private final ProjectDetailService projectDetailService;
+//
+//    @Autowired
+//    public ProjectDetailController(ProjectDetailService projectDetailService) {
+//        this.projectDetailService = projectDetailService;
+//    }
+//
+//    @GetMapping("/show")
+//    public MyCustomResponse<ProjectDetail> getProjectDetailById(@RequestParam Integer projectId) {
+//        ProjectDetail projectDetail = projectDetailService.getProjectDetails(projectId);
+//
+//        return new MyCustomResponse<>(
+//                200,
+//                "Project details retrieved successfully",
+//                projectDetail
+//        );
+//    }
+//}
