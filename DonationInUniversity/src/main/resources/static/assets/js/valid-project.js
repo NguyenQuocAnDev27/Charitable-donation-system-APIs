10dbc04504d15c89da213401909e34bcac84645a
Kien-Huy <113742104+Kien-Huy@users.noreply.github.com>
2024-11-27 19:11:17 +0700

    fix bug

--- DonationInUniversity/src/main/resources/static/assets/js/valid-project.js
+++ DonationInUniversity/src/main/resources/static/assets/js/valid-project.js
@@ -22,9 +22,7 @@ document.getElementById('goalAmount').addEventListener('input', function () {
 
 // Kiểm tra khi người dùng nhập liệu vào trường currentAmount
 document.getElementById('currentAmount').addEventListener('input', function () {
-    const goalAmountInput = document.getElementById('goalAmount');
     const currentAmountInput = this;
-    const goalAmount = goalAmountInput.value;
     const currentAmount = currentAmountInput.value;
 
     // Kiểm tra cả hai trường đều là số hợp lệ
@@ -38,24 +36,6 @@ document.getElementById('currentAmount').addEventListener('input', function () {
     } else {
         currentAmountInput.classList.remove('is-invalid');
         document.getElementById('amount-number-error').style.display = 'none'; // Ẩn thông báo lỗi số không hợp lệ nếu hợp lệ
-    }
-
-    // Kiểm tra nếu cả hai đều là số và Current Amount lớn hơn Goal Amount
-    if (isGoalAmountValid && isCurrentAmountValid) {
-        const goalAmountValue = parseFloat(goalAmount);
-        const currentAmountValue = parseFloat(currentAmount);
-
-        // Lỗi nếu Current Amount lớn hơn Goal Amount
-        if (currentAmountValue > goalAmountValue) {
-            currentAmountInput.classList.add('is-invalid');
-            document.getElementById('amount-goal-error').style.display = 'block'; // Hiển thị lỗi lớn hơn Goal Amount
-        } else {
-            currentAmountInput.classList.remove('is-invalid');
-            document.getElementById('amount-goal-error').style.display = 'none'; // Ẩn thông báo lỗi nếu hợp lệ
-        }
-    } else {
-        // Ẩn lỗi "greater than Goal Amount" nếu số không hợp lệ
-        document.getElementById('amount-goal-error').style.display = 'none';
     }
 });
 
@@ -83,17 +63,6 @@ document.querySelector('form').addEventListener('submit', function (event) {
         event.preventDefault(); // Ngăn form không được gửi đi nếu có lỗi
     }
 
-    // Kiểm tra nếu Current Amount lớn hơn Goal Amount
-    if (isGoalAmountValid && isCurrentAmountValid) {
-        const goalAmountValue = parseFloat(goalAmount);
-        const currentAmountValue = parseFloat(currentAmount);
-
-        if (currentAmountValue > goalAmountValue) {
-            currentAmountInput.classList.add('is-invalid');
-            document.getElementById('amount-goal-error').style.display = 'block';
-            event.preventDefault(); // Ngăn form không được gửi đi nếu Current Amount lớn hơn Goal Amount
-        }
-    }
 });
 document.getElementById('endDate').addEventListener('input', function () {
     const startDateInput = document.getElementById('startDate');
